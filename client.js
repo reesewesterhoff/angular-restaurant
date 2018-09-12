cfed77e3d32760b48a41a33024ec4c4142c7b0e9
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-12 14:07:01 -0500

    appending to DOM correctly

--- client.js
+++ client.js
@@ -1,1 +1,12 @@
 console.log('js loaded');
+
+let foodApp = angular.module('FoodApp', []);
+
+foodApp.controller('FoodController', function(){
+    let self = this;
+
+    self.foods = [
+        {name: 'taco'},
+        {name: 'potato'}
+    ];
+});
